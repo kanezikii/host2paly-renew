6ce3b663a84447fe308a37c95a5bb5762de82949
kanezikii <peng320829@gmail.com>
2026-08-13 08:09:57 +0800

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=bec1fb82-2aee-4267-9be8-7af9cccdcf2c",
+    "https://host2play.gratis/server/renew?i=1bd50741-db92-4981-9019-bac0405b80af",
     
 ]
 
